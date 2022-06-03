e1344006415529f0b5426b11dab5668760c2a646
LDR Laptop <dludnai@yahoo.com>
2022-06-03 11:25:24 +0300

    03.06.2022 -> Displaying different components when the user is logged in or logs out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,42 @@
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 // Styles & images
 import './Navbar.css';
 import Temple from '../assets/temple.svg';
 
+
 export default function Navbar() {
 
     const { logout, isPending } = useLogout();
+    const { user } = useAuthContext();
 
     return (
         <div className="navbar">
-            <ul>
-                <li className="logo">
-                    <img src={Temple} alt="Logo" />
-                    <span>Project Management App</span>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/signup">Register</Link>
-                </li>
-                <li>
-                    {!isPending && <button className="btn" onClick={logout}>Logout</button>}
-                    {isPending && <button className="btn" disabled>Signing out...</button>}
-                </li>
-            </ul>
+                <ul>
+                    <li className="logo">
+                        <img src={Temple} alt="Logo" />
+                        <span>Project Management App</span>
+                    </li>
+                    {!user && (
+                        <>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                            <li>
+                                <Link to="/signup">Register</Link>
+                            </li>
+                        </>
+                    )}
+                    
+                    {user && (
+                    <li>
+                        {!isPending && <button className="btn" onClick={logout}>Logout</button>}
+                        {isPending && <button className="btn" disabled>Signing out...</button>}
+                    </li>
+                    )}
+                </ul>
         </div>
     )
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,6 @@
 import { NavLink } from 'react-router-dom';
+import Avatar from './Avatar';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 //Styles & images
 import './Sidebar.css';
@@ -6,12 +8,15 @@ import DashboardIcon from '../assets/dashboard_icon.svg';
 import AddIcon from '../assets/add_icon.svg';
 
 export default function Sidebar() {
+
+    const { user } = useAuthContext();
+
     return (
         <div className="sidebar">
             <div className="sidebar-content">
                 <div className="user">
-                    {/* Avatar and username here */}
-                    <p>Hey user</p>
+                    <Avatar src={user.photoURL} />
+                    <p>Hi, {user.displayName}</p>
                 </div>
                 <nav className="links">
                     <ul>
