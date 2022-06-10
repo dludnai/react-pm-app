d54c72caa7ce1661eda4005b431034cdb90392c0
LDR Laptop <dludnai@yahoo.com>
2022-06-10 12:07:21 +0300

    10.06.2022 -> Listing comments and deleting projects when complete

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { timestamp } from "../../firebase/config";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useFirestore } from "../../hooks/useFirestore";
+import Avatar from "../../components/Avatar";
 
 export default function ProjectComments({ project }) {
 
@@ -35,6 +36,25 @@ export default function ProjectComments({ project }) {
     return (
         <div className="project-comments">
             <h4>Project Comments</h4>
+
+            {/* Listing comments */}
+            <ul>
+                {project.comments.length > 0 && project.comments.map(comment => (
+                    <li key={comment.id}>
+                        <div className="comment-author">
+                            <Avatar src={comment.photoURL} />
+                            <p>{comment.displayName}</p>
+                        </div>
+                        <div className="comment-date">
+                            <p>Date</p>
+                        </div>
+                        <div className="comment-content">
+                            <p>{comment.content}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+
             <form className="add-comment" onSubmit={handleSubmit}>
                 <label>
                     <span>Add new comment:</span>
